1ac96aca3109764c6b86b2debee8b2e5447dbd2c
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2024-02-20 14:08:32 +0100

    .

--- src/components/VariantB.jsx
+++ src/components/VariantB.jsx
@@ -2,13 +2,15 @@ import image from "../assets/colors.webp";
 
 export default function VariantB() {
   return (
-    <section className={`w-screen overflow-hidden`}>
-      <img
-        className="w-full h-[40rem]"
-        src={image}
-        alt="Image filled with soft colors with a diamond-texture on top"
-        title="Colorful image with diamond texture"
-      />
+    <section className={`w-screen max-h-[40rem] max-w-[90rem]`}>
+      <div className="overflow-hidden">
+        <img
+          className="w-[100%] max-h-[40rem] object-cover filter brightness-90 contrast-110 grayscale-10"
+          src={image}
+          alt="Image filled with soft colors with a diamond-texture on top"
+          title="Colorful image with diamond texture"
+        />
+      </div>
     </section>
   );
 }
